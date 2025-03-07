c468bcf12b48a6d90d8d3bf8b056ce7933d773a6
AramirRp <148534432+AramirRp@users.noreply.github.com>
2025-03-07 10:33:51 +0100

    suite routing

--- web/app/(auth)/(costumer)/(tabs)/sign-up.tsx
+++ web/app/(auth)/(costumer)/(tabs)/sign-up.tsx
@@ -1,12 +1,12 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 
-const inventary = () => {
+const signUp = () => {
   return (
     <View>
-      <Text>inventary</Text>
+      <Text>sign-out</Text>
     </View>
   )
 }
 
-export default inventary
+export default signUp
---
+++ web/app/(customer)/(tabs)/order-details/[id].tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const OrderDetails = () => {
+  return (
+    <View>
+      <Text>Order Details</Text>
+    </View>
+  )
+}
+
+export default OrderDetails
