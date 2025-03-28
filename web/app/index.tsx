f972c11dc136deb4ac3c71d1e2a26c34a3705a25
AramirRp <148534432+AramirRp@users.noreply.github.com>
2025-03-28 15:48:07 +0100

    Update index.tsx

--- web/app/index.tsx
+++ web/app/index.tsx
@@ -3,53 +3,6 @@ import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "expo-router";
 
-export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: "#A1CEDC", dark: "#1D3D47" }}
-      headerImage={
-        <Image
-          source={require("@/assets/images/partial-react-logo.png")}
-          style={styles.reactLogo}
-        />
-      }
-    >
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-      </ThemedView>
-      <View style={styles.buttonContainer}>
-        <Text>Brewery</Text>
-        <Link href="/(brewery)" asChild>
-          <Button title="Go to Brewery" />
-        </Link>
-      </View>
-
-      <View style={styles.buttonContainer}>
-        <Text>API TEST</Text>
-        <Button
-          title="FETCH API TEST"
-          onPress={() => {
-            fetch("http://localhost:3000/test", {
-              method: "GET",
-              headers: {
-                "Content-Type": "application/json",
-              },
-            })
-              .then((res) => res.json())
-              .then((data) => console.log(data));
-          }}
-        />
-      </View>
-
-      <View style={styles.buttonContainer}>
-        <Text>Custodedededmer</Text>
-        <Link href="/(customer)" asChild>
-          <Button title="Go to Custededomer" />
-        </Link>
-      </View>
-    </ParallaxScrollView>
-  );
-}
 type Nav = {
     navigate: (value: string) => void
 }
@@ -179,3 +132,9 @@ const styles = StyleSheet.create({
 });
 
 export default index;
+
+
+
+
+
+
