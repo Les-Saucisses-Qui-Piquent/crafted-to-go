84f1ffb3b1f134c72172d77f91332e25c095f2d1
Tim <tterrassier@innosys.fr>
2025-04-11 14:50:47 +0200

    fix linter

--- scripts/wait.ts
+++ scripts/wait.ts
@@ -1,10 +1,9 @@
-
 const run = async () => {
-    console.log('-== Waiting for db ==-');
-    await new Promise((resolve) => {
-        setTimeout(resolve, 4000);
-    });
+  console.log("-== Waiting for db ==-");
+  await new Promise((resolve) => {
+    setTimeout(resolve, 4000);
+  });
 
-    console.log('-== Finished wainting ==-');
+  console.log("-== Finished wainting ==-");
 };
 run();
