31f2f7b437dee60c09291816a394cdadfc0f2f91
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-04-25 10:52:16 +0200

    feat (test route): Insert new record (#11)

--- api/src/test.routes.ts
+++ api/src/test.routes.ts
@@ -1,10 +1,27 @@
-import { PrismaClient } from "@prisma/client";
-import { FastifyInstance } from "fastify";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { FastifyInstance, FastifyRequest } from "fastify";
+
+type Test = Prisma.testCreateInput;
 
 export default async function (fastify: FastifyInstance) {
   fastify.get("/test", async (_request, response) => {
     const prisma = new PrismaClient();
     const test = await prisma.test.findMany();
     response.send(test);
+  });
+
+  fastify.post("/test", async (request: FastifyRequest<{ Body: Test }>, response) => {
+    const prisma = new PrismaClient();
+    try {
+      const input = request.body;
+
+      const test = await prisma.test.create({ data: input });
+      response.send(test);
+    } catch (error) {
+      fastify.log.error(error);
+      response.status(500).send({ message: "Internal server error" });
+    } finally {
+      prisma.$disconnect();
+    }
   });
 }
