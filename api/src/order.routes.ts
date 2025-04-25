eb875a897905078612834cf28d92b91dd20fc62b
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-04-25 11:57:02 +0200

    Wip 2

--- api/src/order.routes.ts
+++ api/src/order.routes.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance, FastifyRequest } from "fastify";
 import { Prisma, PrismaClient } from "@prisma/client";
 
-type Order = Prisma.orderCreateInput;
+type OrderInsert = Prisma.orderCreateInput;
+type OrderUpdate = Prisma.orderUpdateInput;
 
 export default async function (fastify: FastifyInstance) {
   fastify.get("/orders", async (_request, response) => {
@@ -10,7 +11,7 @@ export default async function (fastify: FastifyInstance) {
       const orders = await prisma.order.findMany();
       response.send(orders);
     } catch (error) {
-      console.error(error);
+      fastify.log.error(error);
       response.status(500).send({ message: "Internal server error" });
     } finally {
       prisma.$disconnect();
@@ -18,39 +19,85 @@ export default async function (fastify: FastifyInstance) {
   });
 
   fastify.get(
-    "/order/:id",
+    "/orders/:id",
     async (request: FastifyRequest<{ Params: { id: string } }>, response) => {
       const prisma = new PrismaClient();
-      const { id } = request.params;
+      try {
+        const { id } = request.params;
 
-      if (!id) {
-        response.status(400).send({ message: "Order ID is required" });
-        return;
+        if (!id) {
+          response.status(400).send({ message: "Order ID is required" });
+          return;
+        }
+
+        const order = await prisma.order.findUnique({
+          where: { id },
+        });
+
+        if (!order) {
+          fastify.log.warn({ id }, "Order not found");
+          response.status(404).send({ message: "Order not found" });
+        } else {
+          response.send(order);
+        }
+      } catch (error) {
+        fastify.log.error(error);
+        response.status(500).send({ message: "Internal server error" });
+      } finally {
+        prisma.$disconnect();
       }
-
-      const order = await prisma.order.findUnique({
-        where: { id },
-      });
-
-      if (!order) {
-        response.status(404).send({ message: "Order not found" });
-      } else {
-        response.send(order);
-      }
-      prisma.$disconnect();
     },
   );
 
-  fastify.post("/order", async (request: FastifyRequest<{ Body: Order }>, response) => {
+  fastify.post("/orders", async (request: FastifyRequest<{ Body: OrderInsert }>, response) => {
     const prisma = new PrismaClient();
 
-    const order = request.body;
+    try {
+      const order = request.body;
 
-    const newOrder = await prisma.order.create({
-      data: {
-        order,
-      },
-    });
-    response.send(newOrder);
+      const newOrder = await prisma.order.create({
+        data: order,
+      });
+
+      response.send(newOrder);
+    } catch (error) {
+      fastify.log.error(error);
+      response.status(500).send({ message: "Internal server error" });
+    } finally {
+      prisma.$disconnect();
+    }
   });
+
+  fastify.put(
+    "/orders/:id",
+    async (request: FastifyRequest<{ Params: { id: string }; Body: OrderUpdate }>, response) => {
+      const prisma = new PrismaClient();
+      try {
+        const { id } = request.params;
+        const valuesToUpdate = request.body;
+
+        if (!id) {
+          response.status(400).send({ message: "Order ID is required" });
+          return;
+        }
+
+        const updatedOrder = await prisma.order.update({
+          where: { id },
+          data: valuesToUpdate,
+        });
+
+        if (!updatedOrder) {
+          fastify.log.warn("Order not found");
+          response.status(404).send({ message: "Order not found" });
+        } else {
+          response.send(updatedOrder);
+        }
+      } catch (error) {
+        fastify.log.error(error);
+        response.status(500).send({ message: "Internal server error" });
+      } finally {
+        prisma.$disconnect();
+      }
+    },
+  );
 }
