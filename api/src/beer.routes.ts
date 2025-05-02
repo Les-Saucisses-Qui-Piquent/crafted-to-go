ac885095469ee02ad0d9d806fce783a9d35ccb3d
Tim <tterrassier@innosys.fr>
2025-05-02 10:25:38 +0200

    ajout beer routes

---
+++ api/src/beer.routes.ts
@@ -0,0 +1,108 @@
+import { Prisma, PrismaClient } from "@prisma/client";
+import { FastifyInstance, FastifyRequest } from "fastify";
+
+type BeerInsert = Prisma.beerCreateInput;
+type BeerUpdate = Prisma.beerUpdateInput;
+
+export default async function (fastify: FastifyInstance) {
+  // Route GET pour récupérer toutes les bières
+  fastify.get("/beers", async (_request, response) => {
+    const prisma = new PrismaClient();
+    try {
+      const beers = await prisma.beer.findMany();
+      response.send(beers);
+    } catch (error) {
+      fastify.log.error(error);
+      response.status(500).send({ message: "Server Error", error });
+    } finally {
+      prisma.$disconnect();
+    }
+  });
+
+  // Route GET pour récupérer une bière
+  fastify.get(
+    "/beers/:id",
+    async (request: FastifyRequest<{ Params: { id: string } }>, response) => {
+      const prisma = new PrismaClient();
+      const { id } = request.params;
+      try {
+        const beer = await prisma.beer.findUnique({
+          where: { id: id },
+        });
+        if (!beer) {
+          fastify.log.warn({ id }, "Beer not found");
+          response.status(404).send({ message: "Beer not found" });
+          return;
+        }
+        response.send(beer);
+      } catch (error) {
+        fastify.log.error(error);
+        response.status(500).send({ message: "Server Error", error });
+      } finally {
+        prisma.$disconnect();
+      }
+    },
+  );
+
+  // Route POST pour ajouter une bière
+  fastify.post("/beers", async (request: FastifyRequest<{ Body: BeerInsert }>, response) => {
+    const prisma = new PrismaClient();
+    try {
+      const input = request.body;
+      const beer = await prisma.beer.create({ data: input });
+      response.send(beer);
+    } catch (error) {
+      fastify.log.error(error);
+      response.status(500).send({ message: "Server Error" });
+    } finally {
+      prisma.$disconnect();
+    }
+  });
+
+  // Route PUT pour modifier une bière
+  fastify.put(
+    "/beers/:id",
+    async (request: FastifyRequest<{ Params: { id: string }; Body: BeerUpdate }>, response) => {
+      const prisma = new PrismaClient();
+      const { id } = request.params;
+      const data = request.body;
+      try {
+        const beer = await prisma.beer.update({
+          where: { id: id },
+          data: data,
+        });
+        if (!beer) {
+          response.status(404).send({ message: "Beer not found" });
+          return;
+        }
+        response.send(beer);
+      } catch (error) {
+        fastify.log.error(error);
+        response.status(500).send({ message: "Server Error", error });
+      } finally {
+        prisma.$disconnect();
+      }
+    },
+  );
+
+  // Route DELETE pour supprimer une bière
+  fastify.delete(
+    "/beers/:id",
+    async (request: FastifyRequest<{ Params: { id: string } }>, response) => {
+      const prisma = new PrismaClient();
+      const { id } = request.params;
+      try {
+        const beer = await prisma.beer.delete({
+          where: { id: id },
+        });
+        fastify.log.warn({ id }, "Deleted beer");
+        response.send({ message: "Beer deleted" });
+      } catch (error) {
+        fastify.log.error(error);
+        response.status(500).send({ message: "Server Error", error });
+      } finally {
+        prisma.$disconnect();
+      }
+    },
+  );
+}
