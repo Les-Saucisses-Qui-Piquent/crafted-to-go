7b5ddda5bec0f5483355d56a648d4ed6bc2ba29d
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-05-16 17:48:37 +0200

    docs(types): add JSDoc for authUser in FastifyRequest

--- api/types.d.ts
+++ api/types.d.ts
@@ -1,6 +1,10 @@
 import "fastify";
 import type { JwtPayload } from "jsonwebtoken";
 
+/**
+ * Decorate and augments the FastifyRequest with token infos
+ * This will render authUser available on all authenticated routes
+ */
 declare module "fastify" {
   interface FastifyRequest {
     authUser: JwtPayload | string;
