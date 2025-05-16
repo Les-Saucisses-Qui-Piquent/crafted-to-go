519ba45c58265b0eeb46e5c063d65b6048d11f42
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-05-16 12:27:29 +0200

    feat(utils): add string utility

---
+++ api/utils/index.ts
@@ -0,0 +1,7 @@
+export const capitalize = (str: string) => {
+  return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+};
+
+export const allCaps = (str: string) => {
+  return str.toUpperCase();
+};
