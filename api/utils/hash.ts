5890342126eaaef0d30c5a9d6e61e4e6f0acfc36
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-05-22 23:04:38 +0200

    chore: added argon secret

--- api/utils/hash.ts
+++ api/utils/hash.ts
@@ -7,10 +7,15 @@ export const useHash = () => {
    * @returns The hashed string
    */
   const hashPassword = async (password: string) => {
+    if (!process.env.ARGON2_SECRET) {
+      throw new Error("ARGON2_SECRET is not defined");
+    }
+
     return await argon2.hash(password, {
       type: argon2.argon2id,
       timeCost: 3,
       parallelism: 1,
+      secret: Buffer.from(process.env.ARGON2_SECRET),
     });
   };
 
