9b75579ab62431887e06406ac016d6cc532b01ca
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-05-30 16:19:47 +0200

    fix(test): Update health check endpoint to use loopback IP

--- api/tests/health.test.ts
+++ api/tests/health.test.ts
@@ -2,7 +2,7 @@ import { describe, it, expect } from "vitest";
 
 describe("API root & health", () => {
   it("should be up", async () => {
-    const response = await fetch("http://localhost:3000/");
+    const response = await fetch("http://127.0.0.1:3000/");
     expect(response.status).toBe(200);
     expect(await response.json()).toStrictEqual({ status: "ok" });
   });
