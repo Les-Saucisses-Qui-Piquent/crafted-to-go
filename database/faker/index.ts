326add7ee0c415137fe7138195b9f1aa7e0b27ed
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-02 15:37:50 +0200

    fix: logging in faker script

--- database/faker/index.ts
+++ database/faker/index.ts
@@ -1,12 +1,20 @@
 import { UserFactory } from "./user-factory";
 import { PrismaClient } from "@prisma/client";
 
+/* eslint-disable no-console */
+
 const main = async (dbclient: PrismaClient) => {
   console.log("-== Starting faker ==-");
+  try {
+    const userFaker = new UserFactory(dbclient);
+    await userFaker.createMany(10);
 
-  const userFaker = new UserFactory(dbclient);
-
-  await userFaker.createMany(10);
+    console.log("-== Completed fake seeding ==-");
+  } catch (error) {
+    console.error("/!\\ Error while running faker /!\\");
+    console.error(error);
+    process.exit(1);
+  }
 };
 
 const prismaClient = new PrismaClient({
