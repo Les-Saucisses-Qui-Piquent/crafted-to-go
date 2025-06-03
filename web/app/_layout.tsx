16f200f4d11ff445d6924bcf590c814e04e77f01
AramirRp <148534432+AramirRp@users.noreply.github.com>
2025-06-03 10:03:45 +0200

    Test export component to figma (#25)
    * ajout mainbutton
    
    * evolutions et ajout polices
    
    * change path
    
    * clean component
    
    ---------
    
    Co-authored-by: pauline.taillemite <pauline.taillemite@qonto.com>

--- web/app/_layout.tsx
+++ web/app/_layout.tsx
@@ -1,9 +1,25 @@
 import { TopBar } from "@/components/TopBar";
+import { useFonts } from "expo-font";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { Stack } from "expo-router";
 import { SafeAreaProvider } from "react-native-safe-area-context";
+import { View, ActivityIndicator } from "react-native";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    HankenGrotesk: require("../assets/fonts/HankenGrotesk-VariableFont_wght.ttf"),
+    "HankenGrotesk-italic": require("../assets/fonts/HankenGrotesk-Italic-VariableFont_wght.ttf"),
+    // Add other weights/styles as needed
+  });
+
+  if (!fontsLoaded) {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <ActivityIndicator size="large" />
+      </View>
+    );
+  }
+
   return (
     <SafeAreaProvider>
       <AuthProvider>
--- web/app/index.tsx
+++ web/app/index.tsx
@@ -2,139 +2,98 @@ import { View, Text, StyleSheet, Image, TouchableOpacity, Button } from "react-n
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useNavigation } from "expo-router";
+import MainButton from "../components/MainButton";
 
 type Nav = {
-    navigate: (value: string) => void
-}
+  navigate: (value: string) => void;
+};
 
 // Add Welcome Screen
 const index = () => {
-    const { navigate } = useNavigation<Nav>();
+  const { navigate } = useNavigation<Nav>();
 
-    return (
-        <SafeAreaView style={[styles.area, ]}>
-            <View style={[styles.container, ]}>
-                <Text style={styles.title}>Bienvenue</Text>
-                <Text style={[styles.subtitle, { color: "black" }]}>
-                              BIERE COUCOU                </Text>
-           
-                
-                <TouchableOpacity
-                        onPress={() => navigate("login")}>
-                  <View style={[styles.boutton, { flexDirection: "row" }]}>
-                  
-                      <Text style={[styles.loginTitle, {
-                          color: "white"
-                      }]}>Already have account? </Text>
-                      
-                          <Text style={styles.loginSubtitle}>Log In</Text>
-                  </View>
-                </TouchableOpacity>
+  return (
+    <SafeAreaView style={[styles.area]}>
+      <View style={[styles.container]}>
+        <Text style={styles.title}>Bienvenue</Text>
+        <Text style={[styles.subtitle, { color: "black" }]}>BIERE COUCOU</Text>
 
-                <TouchableOpacity
-                        onPress={() => navigate("registerBrewery")}>
-                <View style={[styles.boutton, { flexDirection: "row" }]}>
-                    <Text style={[styles.loginTitle, {
-                        color: "white"
-                    }]}>Vous êtes une brasserie </Text>
-                  
-                        <Text style={styles.loginSubtitle}>REGISTER</Text>
-                </View>
-                </TouchableOpacity>
+        <MainButton onPress={() => navigate("login")} title="LOGIN" isBlack />
 
-                <TouchableOpacity
-                        onPress={() => navigate("registerClient")}>
-                <View style={[styles.boutton, { flexDirection: "row" }]}>
-                    <Text style={[styles.loginTitle, {
-                        color: "white"
-                    }]}>Vous voulez des bières ? </Text>
-                   
-                        <Text style={styles.loginSubtitle}>REGISTER</Text>
-                </View>
-                </TouchableOpacity>
+        <MainButton
+          onPress={() => navigate("registerBrewery")}
+          title="BREWERY REGISTER"
+          isSecondary
+        />
 
-            </View>
-            <View style={styles.bottomContainer}>
-                <Text style={[styles.bottomTitle, ]}>
-                    By continuing, you accept the Terms Of Use and
-                </Text>
-                <TouchableOpacity onPress={() => navigate("login")}>
-                    <Text style={[styles.bottomSubtitle,]}>Privacy Policy.</Text>
-                </TouchableOpacity>
-            </View>
-        </SafeAreaView>
-    );
+        <MainButton onPress={() => navigate("registerClient")} title="CLIENT REGISTER" />
+      </View>
+      <View style={styles.bottomContainer}>
+        <Text style={[styles.bottomTitle]}>By continuing, you accept the Terms Of Use and</Text>
+        <TouchableOpacity onPress={() => navigate("login")}>
+          <Text style={[styles.bottomSubtitle]}>Privacy Policy.</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 const styles = StyleSheet.create({
-    area: {
-        flex: 1,
-    },
-    container: {
-        flex: 1,
-        padding: 16,
-        alignItems: "center",
-        justifyContent: "center",
-        gap : 10,
-    },
-    logo: {
-        width: 72,
-        height: 72,
-        marginBottom: 22,
-        marginTop: -22,
-    },
-    title: {
-        fontSize: 28,
-        fontFamily: "bold",
-        marginVertical: 12,
-        textAlign: "center",
-    },
-    subtitle: {
-        fontSize: 12,
-        fontFamily: "regular",
-        color: "black",
-        textAlign: "center",
-        paddingHorizontal: 16,
-    },
-    loginTitle: {
-        fontSize: 14,
-        fontFamily: "regular",
-    },
-    loginSubtitle: {
-        fontSize: 14,
-        fontFamily: "semiBold",
-        color: "yellow",
-    },
-    bottomContainer: {
-        position: "absolute",
-        bottom: 32,
-        right: 0,
-        left: 0,
-        alignItems: "center",
-    },
-    bottomTitle: {
-        fontSize: 12,
-        fontFamily: "regular",
-    },
-    bottomSubtitle: {
-        fontSize: 12,
-        fontFamily: "regular",
-        textDecorationLine: "underline",
-        marginTop: 2,
-    },
-    boutton: {
-      width:400,
-      height:50,
-      padding:10,
-      backgroundColor:"red",
-      borderRadius:15,
-    }
+  area: {
+    flex: 1,
+  },
+  container: {
+    flex: 1,
+    padding: 16,
+    alignItems: "center",
+    justifyContent: "center",
+    gap: 10,
+  },
+  logo: {
+    width: 72,
+    height: 72,
+    marginBottom: 22,
+    marginTop: -22,
+  },
+  title: {
+    fontSize: 28,
+    fontFamily: "bold",
+    marginVertical: 12,
+    textAlign: "center",
+  },
+  subtitle: {
+    fontSize: 12,
+    fontFamily: "regular",
+    color: "black",
+    textAlign: "center",
+    paddingHorizontal: 16,
+  },
+  loginTitle: {
+    fontSize: 14,
+    fontFamily: "regular",
+  },
+  loginSubtitle: {
+    fontSize: 14,
+    fontFamily: "semiBold",
+    color: "yellow",
+  },
+  bottomContainer: {
+    position: "absolute",
+    bottom: 32,
+    right: 0,
+    left: 0,
+    alignItems: "center",
+  },
+  bottomTitle: {
+    fontSize: 12,
+    fontFamily: "regular",
+  },
+  bottomSubtitle: {
+    fontSize: 12,
+    fontFamily: "regular",
+    textDecorationLine: "underline",
+    marginTop: 2,
+  },
 });
 
 export default index;
-
-
-
-
-
-
