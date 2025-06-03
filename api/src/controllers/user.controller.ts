c6faa6fc93775018b6673dfc05ce74b6d536984a
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-03 12:32:10 +0200

    feat: Implement UserController

---
+++ api/src/controllers/user.controller.ts
@@ -0,0 +1,109 @@
+import { FastifyRequest, FastifyReply } from "fastify";
+import { Prisma, PrismaClient } from "@prisma/client";
+import { z } from "zod";
+
+type UserInsert = Prisma.userCreateInput;
+type UserUpdate = Prisma.userUpdateInput;
+
+export const UserController = {
+  getUsers: async (_request: FastifyRequest, reply: FastifyReply) => {
+    const prisma = new PrismaClient();
+    try {
+      const users = await prisma.user.findMany();
+      reply.send(users);
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ message: "Server Error", error });
+    } finally {
+      await prisma.$disconnect();
+    }
+  },
+
+  getUser: async (request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) => {
+    const prisma = new PrismaClient();
+    const { id } = request.params;
+    try {
+      const { success } = z.string().uuid().safeParse(id);
+      if (!success) {
+        reply.status(400).send({ message: "Invalid uuid" });
+        return;
+      }
+
+      const user = await prisma.user.findUnique({ where: { id } });
+      if (!user) {
+        reply.status(404).send({ message: "User not found" });
+        return;
+      }
+
+      reply.send(user);
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ message: "Server Error", error });
+    } finally {
+      await prisma.$disconnect();
+    }
+  },
+
+  createUser: async (request: FastifyRequest<{ Body: UserInsert }>, reply: FastifyReply) => {
+    const prisma = new PrismaClient();
+    try {
+      const user = await prisma.user.create({ data: request.body });
+      reply.send(user);
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ message: "Server Error" });
+    } finally {
+      await prisma.$disconnect();
+    }
+  },
+
+  updateUser: async (
+    request: FastifyRequest<{ Params: { id: string }; Body: UserUpdate }>,
+    reply: FastifyReply,
+  ) => {
+    const prisma = new PrismaClient();
+    const { id } = request.params;
+    try {
+      const { success } = z.string().uuid().safeParse(id);
+      if (!success) {
+        reply.status(400).send({ message: "Invalid uuid" });
+        return;
+      }
+
+      const user = await prisma.user.update({
+        where: { id },
+        data: request.body,
+      });
+      if (!user) {
+        reply.status(404).send({ message: "User not found" });
+        return;
+      }
+      reply.send(user);
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ message: "Server Error", error });
+    } finally {
+      await prisma.$disconnect();
+    }
+  },
+
+  deleteUser: async (request: FastifyRequest<{ Params: { id: string } }>, reply: FastifyReply) => {
+    const prisma = new PrismaClient();
+    const { id } = request.params;
+    try {
+      const { success } = z.string().uuid().safeParse(id);
+      if (!success) {
+        reply.status(404).send({ message: "User not found" });
+        return;
+      }
+
+      await prisma.user.delete({ where: { id } });
+      reply.send({ message: "User deleted" });
+    } catch (error) {
+      console.error(error);
+      reply.status(500).send({ message: "Server Error", error });
+    } finally {
+      await prisma.$disconnect();
+    }
+  },
+};
