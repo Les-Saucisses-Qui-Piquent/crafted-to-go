95b4c2398397fbf332ff854064353d97c74c5871
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-05 12:23:11 +0200

    feat: decorate Prisma client

--- api/server.ts
+++ api/server.ts
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import server from "./src/index";
+import { PrismaClient } from "@prisma/client";
 
 if (!process.env.FRONTEND_URL) {
   console.error("FRONTEND_URL is not defined. Server will not start.");
@@ -10,12 +11,28 @@ const fastify = Fastify({
   logger: true,
 });
 
+fastify.decorate("prisma", new PrismaClient());
+
 fastify.register(server);
 
-fastify.listen({ host: "0.0.0.0", port: 3000 }, (err, address) => {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  fastify.log.info(`Server listening at ${address}`);
-});
+const start = async () => {
+  fastify.listen({ host: "0.0.0.0", port: 3000 }, (err, address) => {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    fastify.log.info(`Server listening at ${address}`);
+  });
+};
+
+const shutdown = async () => {
+  fastify.log.info("Shutting down server...");
+  await fastify.close();
+  await fastify.prisma.$disconnect();
+  process.exit(0);
+};
+
+process.on("SIGINT", shutdown);
+process.on("SIGTERM", shutdown);
+
+start();
