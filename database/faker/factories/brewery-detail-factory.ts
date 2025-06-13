26760753c116870573d46df93311f3acac0d9225
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-13 16:42:15 +0200

    feat: add opening hours generation to BreweryDetailFactory

--- database/faker/factories/brewery-detail-factory.ts
+++ database/faker/factories/brewery-detail-factory.ts
@@ -4,18 +4,70 @@ import type { FakerImplementation } from "./types";
 
 type BreweryDetail = Prisma.brewery_detailCreateInput;
 
+type Day = "monday" | "tuesday" | "wednesday" | "thursday" | "friday" | "saturday" | "sunday";
+type DayDetail = {
+  isOpen: boolean;
+  openTime: string;
+  closeTime: string;
+};
+type JsonHours = Record<Day, DayDetail>;
+
 export class BreweryDetailFactory implements FakerImplementation {
   constructor(private readonly dbClient: PrismaClient) {}
 
   private generate = (breweryId: string): BreweryDetail => {
+    const getRandomTime = () => {
+      const hours = faker.number.int({ min: 0, max: 12 });
+      const minutes = faker.number.int({ min: 0, max: 59 });
+      return `${hours.toString().padStart(2, "0")}:${minutes.toString().padStart(2, "0")}`;
+    };
+
+    const openingHours: JsonHours = {
+      monday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+      tuesday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+      wednesday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+      thursday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+      friday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+      saturday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+      sunday: {
+        isOpen: faker.datatype.boolean(),
+        openTime: getRandomTime(),
+        closeTime: getRandomTime(),
+      },
+    };
+
     return {
       image: faker.image.url(),
       logo: faker.image.url(),
       description: faker.lorem.paragraph(),
       social_link: faker.internet.url(),
-      opening_hours: faker.lorem.paragraph(),
+      opening_hours: openingHours,
       has_taproom: faker.datatype.boolean(),
-      taproom_hours: faker.lorem.paragraph(),
+      taproom_hours: openingHours,
       phone_number: faker.phone.number(),
       email: faker.internet.email(),
       brewery_fk: {
