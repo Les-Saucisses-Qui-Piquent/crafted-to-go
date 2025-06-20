dd980871dcda0327010244ff8da98a079da988c8
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-06-20 16:50:38 +0200

    feat: Modified root route to serve api-doc

--- api/src/routes/root.routes.ts
+++ api/src/routes/root.routes.ts
@@ -1,11 +1,17 @@
 import { FastifyInstance } from "fastify";
+import path from "path";
+import staticServer from "@fastify/static";
 
 export default async function (fastify: FastifyInstance) {
+  fastify.register(staticServer, {
+    root: path.join(__dirname, "../public"),
+  });
+
   fastify.get("/", async (_request, response) => {
-    response.redirect("/health");
+    return response.sendFile("index.html");
   });
 
   fastify.get("/health", async (_request, response) => {
-    response.send({ status: "ok" });
+    return response.send({ status: "ok" });
   });
 }
