188ce8a1b3975a0d900c60fc6ccec7c52697dfb8
pauline.taillemite <pauline.taillemite@qonto.com>
2025-07-25 12:10:45 +0200

    fix layout

--- web/components/beerCard/BeerCardSmall.tsx
+++ web/components/beerCard/BeerCardSmall.tsx
@@ -24,12 +24,14 @@ export default function BeerCardSmall({
       <Image style={styles.image} source={image} />
       <View style={styles.infoContainer}>
         <Text style={styles.name}>{name}</Text>
-        <View style={styles.abvStyleRow}>
-          {beer_style && <Text style={styles.beerStyle}>{beer_style}</Text>}
-          <Text style={styles.abv}>{abv_rate}%</Text>
+        <View style={styles.bottomInfo}>
+          <View style={styles.abvStyleRow}>
+            {beer_style && <Text style={styles.beerStyle}>{beer_style}</Text>}
+            <Text style={styles.abv}>{abv_rate}%</Text>
+          </View>
+          <Text style={styles.stock}>En stock : {quantity}</Text>
+          <Text style={styles.price}>Prix : {price}€</Text>
         </View>
-        <Text style={styles.stock}>En stock : {quantity}</Text>
-        <Text style={styles.price}> Prix : {price}€</Text>
       </View>
     </View>
   );
@@ -40,51 +42,57 @@ const styles = StyleSheet.create({
     width: 150,
     height: 220,
     backgroundColor: "#fff",
-    borderWidth: 1,
-    borderRadius: 8,
-    shadowColor: "#000",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.1,
-    shadowRadius: 4,
+    //borderWidth: 1,
+    //borderRadius: 8,
+    //shadowColor: "#000",
+    //shadowOffset: { width: 0, height: 2 },
+    //shadowOpacity: 0.1,
+    //shadowRadius: 4,
     elevation: 3,
   },
   image: {
     width: "100%",
     height: 120,
-    borderTopLeftRadius: 8,
-    borderTopRightRadius: 8,
+    //borderTopLeftRadius: 8,
+    //borderTopRightRadius: 8,
   },
   infoContainer: {
     padding: 10,
+    flex: 1,
+    justifyContent: "space-between",
   },
   name: {
     fontSize: 12,
-    fontWeight: "bold",
+    fontWeight: "700",
     color: "#000",
-    marginBottom: 4,
+    textTransform: "capitalize",
+    marginBottom: 6,
+  },
+  bottomInfo: {
+    gap: 4,
   },
   abvStyleRow: {
     flexDirection: "row",
     alignItems: "center",
-    marginBottom: 4,
   },
   beerStyle: {
-    fontSize: 10,
-    color: "#666",
+    fontSize: 12,
+    color: "#000",
     marginRight: 6,
+    textTransform: "capitalize"
   },
   abv: {
-    fontSize: 10,
+    fontSize: 12,
     color: "#000",
   },
   stock: {
-    fontSize: 9,
+    fontSize: 10,
     color: "#888",
-    marginBottom: 4,
+    fontWeight: "300",
   },
   price: {
     fontSize: 10,
-    fontWeight: "600",
-    color: "#000",
+    fontWeight: "300",
+    color: "#888",
   },
 });
