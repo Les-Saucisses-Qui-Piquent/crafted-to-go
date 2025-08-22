cf35d119f921353af44948a479e2608b84e12400
EliDevToBe <158275307+EliDevToBe@users.noreply.github.com>
2025-08-22 11:13:10 +0200

    feat: get order-details from a UserId

--- api/src/interfaces/IOrderDetail.ts
+++ api/src/interfaces/IOrderDetail.ts
@@ -7,6 +7,7 @@ export interface IOrderDetail {
   getOrderDetails: () => Promise<order_detail[]>;
   getOrderDetail: (id: string) => Promise<order_detail | null>;
   getDetailFromOrder: (orderId: string) => Promise<order_detail[]>;
+  getDetailFromUser: (userId: string) => Promise<order_detail[]>;
   createOrderDetail: (payload: OrderDetailInsert) => Promise<order_detail>;
   updateOrderDetail: (id: string, payload: OrderDetailUpdate) => Promise<order_detail>;
   deleteOrderDetail: (id: string) => Promise<order_detail>;
